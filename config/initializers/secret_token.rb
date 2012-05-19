df7745e24ff027534a0d93128b160f29b00eaa64
Evan O'Connell <oconn.e@gmail.com>
2012-05-19 15:46:10 -0400

    init ruby on rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bagmaster::Application.config.secret_token = 'f5939e468918397817ce9488d4e40ef6a97f2d28d622b1f944774a4d48b35c320dbd13dc1b4fd28c06e2152d2d8686d7959c50520720ed0115df42a78cdc1a07'
